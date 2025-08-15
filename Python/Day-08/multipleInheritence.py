2a6f66e2008dd995d0bb28d73d72db01927a9154
Rahul Rudra <rahulrudra665@gmail.com>
2025-08-15 14:34:51 +0600

    changes made

--- Python/Day-08/multipleInheritence.py
+++ Python/Day-08/multipleInheritence.py
@@ -1,11 +1,43 @@
 class Human:
+    def __init__(self, legs):
+        self.num_heart = 1
+        self.legs = legs
+
+    def eat(self, name, num_heart):
+        self.name = name
+        self.num_heart = num_heart
+        print(f'I can eat {self.name}')
+
+
+class Male:
+    def __init__(self, name):
+        self.nose = 3
+        self.name = name
+
+    def flirt(self):
+        self.nose = 1
+        print(f'I can and have {self.nose} nose')
+
+
+class Boy(Human, Male):
+    def __init__(self, name, legs=2):
+        Male.__init__(self, name)
+        Human.__init__(self, legs)
+
+
+boy1 = Boy("Rahul")
+print('18', boy1.name, boy1.legs)
+
+boy1.legs = 1  # change legs
+print("Legs", boy1.legs)
+
+print('18', boy1.num_heart)
+
+boy1.eat("Mango", 1)
+boy1.flirt()
+
+
+class Human1:
     def eat(self):
         print("I can eat")
-class Male:
-    def flirt(self):
-        print("I can Flirt")
-class Boy(Human,Male):
-    pass
-
-boy1=Boy()
-print(boy1.eat())
+class Male(Human1):
